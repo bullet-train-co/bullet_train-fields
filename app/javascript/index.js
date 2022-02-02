13172110fa1e3b173a17687df94f4beff13b6632
Pascal Laliberté <pascal@hey.com>
2022-02-02 16:42:55 +0000

    add file_field_controller to js package

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -2,7 +2,7 @@ import ButtonToggleController from './controllers/fields/button_toggle_controlle
 import CloudinaryImageController from './controllers/fields/cloudinary_image_controller'
 import ColorPickerController from './controllers/fields/color_picker_controller'
 import DateController from './controllers/fields/date_controller'
-// import FileFieldController from './controllers/fields/file_field_controller'
+import FileFieldController from './controllers/fields/file_field_controller'
 // import PhoneController from './controllers/fields/phone_controller'
 // import SuperSelectController from './controllers/fields/super_select_controller'
 
@@ -11,7 +11,7 @@ export {
   CloudinaryImageController,
   ColorPickerController,
   DateController,
-  // FileFieldController,
+  FileFieldController,
   // PhoneController,
   // SuperSelectController
 }
