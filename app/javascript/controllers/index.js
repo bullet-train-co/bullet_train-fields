6f5b33443686bb6f536e9a2b99356cf4174bfedd
Pascal Laliberté <pascal@hey.com>
2022-02-11 15:52:01 +0000

    fix exporting stimulus controllers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,24 +1,36 @@
 import { identifierForContextKey } from "@hotwired/stimulus-webpack-helpers"
 
-export ButtonToggleController from './fields/button_toggle_controller'
-export CloudinaryImageController from './fields/cloudinary_image_controller'
-export ColorPickerController from './fields/color_picker_controller'
-export DateController from './fields/date_controller'
-export FileFieldController from './fields/file_field_controller'
-export PhoneController from './fields/phone_controller'
-export SuperSelectController from './fields/super_select_controller'
+import ButtonToggleController from './fields/button_toggle_controller'
+import CloudinaryImageController from './fields/cloudinary_image_controller'
+import ColorPickerController from './fields/color_picker_controller'
+import DateController from './fields/date_controller'
+import FileFieldController from './fields/file_field_controller'
+import PhoneController from './fields/phone_controller'
+import SuperSelectController from './fields/super_select_controller'
 
 export const controllerDefinitions = [
-  [ButtonToggleController, './fields/button_toggle_controller'],
-  [CloudinaryImageController, './fields/cloudinary_image_controller'],
-  [ColorPickerController, './fields/color_picker_controller'],
-  [DateController, './fields/date_controller'],
-  [FileFieldController, './fields/file_field_controller'],
-  [PhoneController, './fields/phone_controller'],
-  [SuperSelectController, './fields/super_select_controller']
-].map(([controller, key]) => {
+  [ButtonToggleController, 'fields/button_toggle_controller.js'],
+  [CloudinaryImageController, 'fields/cloudinary_image_controller.js'],
+  [ColorPickerController, 'fields/color_picker_controller.js'],
+  [DateController, 'fields/date_controller.js'],
+  [FileFieldController, 'fields/file_field_controller.js'],
+  [PhoneController, 'fields/phone_controller.js'],
+  [SuperSelectController, 'fields/super_select_controller.js']
+].map((d) => {
+  const key = d[1]
+  const controller = d[0]
   return {
     identifier: identifierForContextKey(key),
     controllerConstructor: controller
   }
 })
+
+export default {
+  ButtonToggleController,
+  CloudinaryImageController,
+  ColorPickerController,
+  DateController,
+  FileFieldController,
+  PhoneController,
+  SuperSelectController
+}
